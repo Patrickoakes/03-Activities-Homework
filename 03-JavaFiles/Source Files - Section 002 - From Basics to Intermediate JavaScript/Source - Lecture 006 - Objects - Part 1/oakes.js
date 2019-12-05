6bb6d423d59cde873e4f5920bff293a8939166e7
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-05 07:12:40 -0800

    commit walk thru

--- 03-JavaFiles/Source Files - Section 002 - From Basics to Intermediate JavaScript/Source - Lecture 006 - Objects - Part 1/oakes.js
+++ 03-JavaFiles/Source Files - Section 002 - From Basics to Intermediate JavaScript/Source - Lecture 006 - Objects - Part 1/oakes.js
@@ -27,24 +27,24 @@ PROPERTIES                             METHODS
 
 person.firstname = 'John';
 
-console.log(person);*/
+console.log(person);
 
 
 // CONSOLE gives us {} becasue they always represent an object
 
-/*var person = new Object()
+var person = new Object()
 
 person.firstname = 'John';  //  a dot or period is also an OPERATOR like ARITHMETIC SYMBOL
 
-person['lastname'] = 'Smith'; // []  are also an OPERATOR listed on the precedence table.  
+person['lastname'] = 'Smith'; // []  are also an OPERATOR listed on the precedence table.
 
 console.log(person);
 /////////////
 
-console.log(person.firstname);
+console.log(person.firstname);   // PREFERRED WITH CONSTUCTIVE IS DOT
 
-console.log(person['lastname'])
-//////////////    
+console.log(person['lastname'])   //SECOND WAY IS WITH BRACKETS AND STRING
+//////////////
 var person = new Object()
 
 var job = 'profession'; /// ADD Profession as variable
@@ -53,20 +53,20 @@ var name = 'firstname';//// ADDED THIS VARIABLE
 
 person.name = 'John'; // Changed First name into NAME//
 
-/// person.name = 'Bob';///  ACTIVATE THIS LAST IT CHANGES NAME DYNAMICALLY
+//person.name = 'Bob';///  ACTIVATE THIS LAST IT CHANGES NAME DYNAMICALLY
 
 person['lastname'] = 'Smith';
 
-person[job] = 'instructor';  /// added variable to brackests with new 
+person[job] = 'instructor';  /// added variable to brackests with new
 
 console.log(person);  ///added property to person 'instructor'
 
 console.log(person.firstname);
 
-console.log(person['lastname'])*/
+console.log(person['lastname'])
 
-
-/////NEW CODE//////
+/
+/////NEW CODE//////*/
 
 var person = new Object();
 
