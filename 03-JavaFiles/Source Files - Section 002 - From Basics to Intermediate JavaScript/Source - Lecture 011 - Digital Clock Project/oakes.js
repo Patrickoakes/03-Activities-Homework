9f6075031129654f9aa890cdd8b2cb8a65bc05bc
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-06 20:07:20 -0800

    dig clock

---
+++ 03-JavaFiles/Source Files - Section 002 - From Basics to Intermediate JavaScript/Source - Lecture 011 - Digital Clock Project/oakes.js
@@ -0,0 +1,15 @@
+function digitalClock() {
+  var date = new Date();
+
+  var hours = date.getHours();
+
+  var minutes = date.getMinutes();
+
+  var seconds = date.getSeconds();
+
+  var day = date.getDay();
+
+  console.log(hours);
+}
+
+digitalClock();
